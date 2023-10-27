577cbe543f6d1420fd611b2c6183b3ab64d3fb51
dlydka <lydkadm@gmail.com>
2023-10-27 11:25:41 +0200

    Added age field to the registration form

--- src/forms.py
+++ src/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, IntegerField, RadioField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
 class RegisterForm(FlaskForm):
     username = StringField(label='Username', validators=[DataRequired(), Length(min=3, max=64)])
     name = StringField(label='Name', validators=[DataRequired(), Length(min=3, max=64)])
     surname = StringField(label='Surname', validators=[DataRequired(), Length(min=3, max=64)])
+    age = IntegerField(label="Age", validators=[DataRequired(), NumberRange(min=1, max=150)])
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     gender = RadioField(label='Gender', choices=[('female', 'Female'),
                                                  ('male', 'Male'),
