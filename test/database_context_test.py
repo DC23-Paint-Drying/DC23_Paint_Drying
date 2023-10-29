96333f94521140cc2d666a3e33891ba44972534f
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-10-29 20:33:14 +0100

    Added passing user data from Register and Edit forms to db (#11)
    * added saving user to csv file and editing user
    
    * added test for get_client_by_mail method
    
    * Switched to using get_clients method
    
    * Fix conflicts

--- test/database_context_test.py
+++ test/database_context_test.py
@@ -4,7 +4,8 @@ import unittest
 from src.bundle_info import BundleInfo
 from src.client_info import ClientInfo
 import src.database_context
-from src.user_dto import Gender, UserDto
+from src.user_dto import UserDto
+
 
 class DataBaseContextTests(unittest.TestCase):
     def setUp(self) -> None:
@@ -14,15 +15,16 @@ class DataBaseContextTests(unittest.TestCase):
         self.context.destroy()
 
     def test_read_write(self):
-        user = UserDto("testName", "testFirstName", "testSurname", "test@email.com", Gender("other"))
-        bundles = [BundleInfo(str(uuid.uuid4()), "test@email.com", "YellowPaintPremium", "1980-01-01", "2040-12-20"), BundleInfo(str(uuid.uuid4()), "test@email.com", "NoAds", "1980-01-01", "2040-12-20")]
+        user = UserDto("testName", "testFirstName", "testSurname", "99", "test@email.com", "other")
+        bundles = [BundleInfo(str(uuid.uuid4()), "test@email.com", "YellowPaintPremium", "1980-01-01", "2040-12-20"),
+                   BundleInfo(str(uuid.uuid4()), "test@email.com", "NoAds", "1980-01-01", "2040-12-20")]
         info = ClientInfo(user, "basic", bundles)
         assert user.username == "testName"
-        assert user.name== "testFirstName"
+        assert user.name == "testFirstName"
         assert user.surname == "testSurname"
+        assert user.age == "99"
         assert user.email == "test@email.com"
-        assert user.gender == Gender("other")
-
+        assert user.gender == "other"
 
         assert len(bundles) == 2
 
@@ -33,10 +35,11 @@ class DataBaseContextTests(unittest.TestCase):
         client = self.context.get_client_by_email("test@email.com")
 
         assert client.basic.username == "testName"
-        assert client.basic.name== "testFirstName"
+        assert client.basic.name == "testFirstName"
         assert client.basic.surname == "testSurname"
+        assert client.basic.age == "99"
         assert client.basic.email == "test@email.com"
-        assert client.basic.gender == Gender("other")
+        assert client.basic.gender == "other"
 
         assert client.subscription == "basic"
 
@@ -47,4 +50,3 @@ class DataBaseContextTests(unittest.TestCase):
 
         assert client.bundles[0].name == "YellowPaintPremium"
         assert client.bundles[1].name == "NoAds"
-        
