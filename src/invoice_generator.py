58f2cbd8cc5c42b855acb2628ed67361154a19de
Lenetis <70149832+Lenetis@users.noreply.github.com>
2023-10-29 21:04:55 +0100

    Changed datetime to date where possible

--- src/invoice_generator.py
+++ src/invoice_generator.py
@@ -5,7 +5,7 @@ import datetime
 from typing import Dict
 
 
-def generate_invoice_xml(client_data: Dict, date: datetime.datetime | None = None, indent: str = "    ") -> str:
+def generate_invoice_xml(client_data: Dict, date: datetime.date | None = None, indent: str = "    ") -> str:
     """
     Generates an invoice in XML format. Creates a file named "invoice-{invoice_number}.xml" and returns its filename.
     The invoice payment is due to the first day of the next month, relative to the invoice's date.
@@ -42,7 +42,7 @@ def generate_invoice_xml(client_data: Dict, date: datetime.datetime | None = Non
 
     payment = ET.SubElement(invoice, "payment")
     ET.SubElement(payment, "amount").text = "29.99"
-    ET.SubElement(payment, "due").text = str(datetime.datetime(year=date.year, month=date.month+1, day=1).date())
+    ET.SubElement(payment, "due").text = str(datetime.date(year=date.year, month=date.month+1, day=1))
 
     tree = ET.ElementTree(invoice)
     ET.indent(tree, indent)
