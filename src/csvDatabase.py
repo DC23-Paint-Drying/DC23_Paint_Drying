efc297b2e2601db3bdd16b878f486301793577f6
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2023-10-29 14:56:03 +0100

    added missing docstring

--- src/csvDatabase.py
+++ src/csvDatabase.py
@@ -48,6 +48,12 @@ class CSVDatabase:
             self._fields = reader.fieldnames
 
     def get_entry_by_field(self, key: str, value: str) -> list[dict]:
+        """
+        Returns entries that matches provided key-value pair
+
+        :param key: name of column to be searched
+        :param value: value of client's data field to fetch from the database
+        """
         with open(self._filename, 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile, fieldnames=self._fields)
             output = []
