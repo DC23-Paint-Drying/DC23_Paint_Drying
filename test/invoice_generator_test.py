65f880da76ef5ef3c7aeadf2b75f961ec6f9356e
Lenetis <70149832+Lenetis@users.noreply.github.com>
2023-11-03 14:17:23 +0100

    Added test for the deprecated function because github test coverage complained

--- test/invoice_generator_test.py
+++ test/invoice_generator_test.py
@@ -50,6 +50,44 @@ def test_generate_invoice():
     os.remove(filename)
 
 
+def test_generate_invoice_deprecated():
+    user_data = {
+        "name": "John",
+        "surname": "Smith",
+        "mail": "john@smi.th",
+        "subscription": "bronze",
+        "packages": [
+            ["Paint+", 29.99]
+        ]
+    }
+
+    invoice_date = datetime.date(year=2023, month=10, day=1)
+
+    filename = invoice_generator.generate_invoice_xml(user_data, date=invoice_date)
+    invoice_number = filename[8:-4]
+
+    tree = ET.parse(filename)
+
+    assert tree.find("./invoice-number").text == invoice_number
+    assert tree.find("./date").text == str(invoice_date)
+
+    # todo test company data when it is standardized (written in a constants file for example)
+
+    assert tree.find("./client/name").text == "John"
+    assert tree.find("./client/surname").text == "Smith"
+
+    assert tree.find("./client/name").text == "John"
+    assert tree.find("./client/surname").text == "Smith"
+    assert tree.find("./client/mail").text == "john@smi.th"
+    assert tree.find("./client/subscription/type").text == "bronze"
+    assert tree.find("./client/packages/package/type").text == "Paint+"
+    assert tree.find("./client/packages/package/cost").text == "29.99"
+
+    assert tree.find("./payment/due").text == "2023-11-01"
+
+    os.remove(filename)
+
+
 # requires the user to have wkhtmltopdf installed, otherwise raises: OSError: No wkhtmltopdf executable found: "b''"
 @pytest.mark.skip(reason="Shouldn't be run automatically")
 def test_generate_pdf():
