d0c95f6ef84943fc5e696742ffc8055d3c87b870
furkacz <s184727@student.pg.edu.pl>
2023-11-03 20:06:12 +0100

    Reenabled chart generation tests

--- src/report/report_utils.py
+++ src/report/report_utils.py
@@ -11,7 +11,7 @@ from docx.table import Table, _Cell
 from docx.enum import table, text
 from docx.shared import Emu, Pt, RGBColor
 from docx.oxml.shared import qn, OxmlElement
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import subplots, Circle, savefig
 
 
 def create_stylised_document() -> Document:
@@ -257,13 +257,13 @@ def create_donut_chart(values: [int], labels: [str]) -> str:
     if len(values) != len(labels):
         raise ValueError('Each value must have exactly one label')
 
-    fig, ax = plt.subplots()
+    fig, ax = subplots()
     fig.set_figwidth(6.0)
     fig.set_figheight(6.0)
     _, texts = ax.pie(values, labels=labels, textprops={'weight': 'bold'}, labeldistance=0.75, startangle=180.0)
     for t in texts:
         t.set_horizontalalignment('center')
-    ax.add_artist(plt.Circle((0, 0), 0.5, color='White'))
+    ax.add_artist(Circle((0, 0), 0.5, color='White'))
     filename = f'{uuid.uuid4()}.png'
-    plt.savefig(filename, bbox_inches='tight', pad_inches=-0.3)
+    savefig(filename, bbox_inches='tight', pad_inches=-0.3)
     return filename
