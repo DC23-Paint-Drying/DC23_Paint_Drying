7faa910e6d9bc5268206bfa90f046376de9ae85d
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2023-11-05 16:46:38 +0100

    review fix

--- src/client_info.py
+++ src/client_info.py
@@ -11,12 +11,12 @@ class ClientInfo:
     subscription: subscription_info.SubscriptionInfo
     bundles: list[bundle_info.BundleInfo]
 
-    def to_json(self, prety_print: bool = True) -> str:
+    def to_json(self, pretty_print: bool = True) -> str:
         data = {}
         data['basic'] = json.loads(self.basic.to_json())
         data['subscription'] = json.loads(self.subscription.to_json())
         data['bundles'] = [json.loads(bundle.to_json()) for bundle in self.bundles]
-        if prety_print:
+        if pretty_print:
             del data['basic']['timestamp']
             del data['basic']['id']
             del data['subscription']['subscription_timestamp']
