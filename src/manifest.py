339844263fac831ebe293d105a36ec763043892b
Lenetis <70149832+Lenetis@users.noreply.github.com>
2023-11-12 19:28:43 +0100

    Small manifest changes

--- src/manifest.py
+++ src/manifest.py
@@ -5,34 +5,35 @@ COMPANY_NIP = "123-456-78-90"
 COMPANY_BANK_ACCOUNT = "12 3456 7890 0000 0000 1234 5678"
 
 SUBSCRIPTIONS = {
-    "basic":{
-        "name": "Podstawowy",
+    "basic": {
+        "name": "Podstawowa",
         "price": 12.99
     },
-    "standard":{
+    "standard": {
         "name": "Standardowy",
         "price": 39.99
     },
-    "premium":{
+    "premium": {
         "name": "Premium",
         "price": 56.99
     }
 }
 
 PACKETS = {
-    "monthly":{
+    "monthly": {
         "name": "Miesięczny",
         "description": "Ten pakiet oferuje rotacyjny wybór comiesięcznych scen schnięcia farby, dzięki czemu zawartość jest świeża i ekscytująca.",
         "duration": 31,
         "price": 9.99
     },
-    "family":{
+    "family": {
         "name": "Rodzinny",
-        "description": "Ten pakiet, przeznaczona dla rodzin, umożliwia wielu profilom korzystanie z treści związanych z schnięciem farby na jednym koncie.",
+        "description": "Ten pakiet, przeznaczony dla rodzin, umożliwia wielu profilom korzystanie z treści związanych ze schnięciem farby na jednym koncie.",
         "duration": 31,
         "price": 12.99
     }
 }
+
 
 class USER_TYPES:
     ADMIN = "admin"
