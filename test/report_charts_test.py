d841ad39558bb592257dbad26d0ee730ad150f04
furkacz <s184727@student.pg.edu.pl>
2023-11-12 17:57:31 +0100

    Changing PyTest fixtures to TestCase

--- test/report_charts_test.py
+++ test/report_charts_test.py
@@ -1,37 +1,31 @@
 from os import remove, listdir
-
-import pytest
+import unittest
 
 from src.report.report_charts import create_donut_chart
 
 
-@pytest.fixture(autouse=True)
-def cleanup_files():
-    yield
-    for file in listdir('./'):
-        if file.endswith('.png'):
-            remove(file)
+class ReportCharts(unittest.TestCase):
+    def tearDown(self) -> None:
+        for file in listdir('./'):
+            if file.endswith('.png'):
+                remove(file)
 
+    def test_create_donut_chart(self) -> None:
+        create_donut_chart([1, 1], ['a', 'b'])
 
-def test_create_donut_chart():
-    create_donut_chart([1, 1], ['a', 'b'])
+    def test_create_donut_chart_with_no_values(self) -> None:
+        with self.assertRaises(ValueError):
+            create_donut_chart([], ['a', 'b'])
 
+    def test_create_donut_chart_with_no_labels(self) -> None:
+        with self.assertRaises(ValueError):
+            create_donut_chart([1, 2], [])
 
-def test_create_donut_chart_with_no_values():
-    with pytest.raises(ValueError):
-        create_donut_chart([], ['a', 'b'])
+    def test_create_donut_chart_with_not_enough_labels(self) -> None:
+        with self.assertRaises(ValueError):
+            create_donut_chart([1, 2], ['a'])
 
+    def test_create_donut_chart_with_too_many_labels(self) -> None:
+        with self.assertRaises(ValueError):
+            create_donut_chart([1, 2], ['a', 'b', 'c'])
 
-def test_create_donut_chart_with_no_labels():
-    with pytest.raises(ValueError):
-        create_donut_chart([1, 2], [])
-
-
-def test_create_donut_chart_with_not_enough_labels():
-    with pytest.raises(ValueError):
-        create_donut_chart([1, 2], ['a'])
-
-
-def test_create_donut_chart_with_too_many_labels():
-    with pytest.raises(ValueError):
-        create_donut_chart([1, 2], ['a', 'b', 'c'])
--- test/report_convert_to_pdf_test.py
+++ test/report_convert_to_pdf_test.py
@@ -1,26 +1,24 @@
-from os import remove, listdir, environ
+from os import remove, listdir
 from os.path import exists
+import unittest
 
-import pytest
 from docx import Document
 
 from src.report.report_utils import convert_docx_to_pdf
 
 
-@pytest.fixture(autouse=True)
-def file_cleanup():
-    yield
-    for file in listdir('./'):
-        if file.endswith('.docx'):
-            remove(file)
-        if file.endswith('.pdf'):
-            remove(file)
+class ReportPDFConvertTests(unittest.TestCase):
+    def tearDown(self) -> None:
+        for file in listdir('./'):
+            if file.endswith('.docx'):
+                remove(file)
+            if file.endswith('.pdf'):
+                remove(file)
 
+    def test_convert_docx_to_pdf(self) -> None:
+        document = Document()
+        document.save('test.docx')
+        assert exists('test.docx')
 
-def test_convert_docx_to_pdf():
-    document = Document()
-    document.save('test.docx')
-    assert exists('test.docx')
-
-    convert_docx_to_pdf('test.docx', '')
-    assert exists('test.pdf')
+        convert_docx_to_pdf('test.docx', '')
+        assert exists('test.pdf')
