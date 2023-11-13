a7ded6dd7e68cae7476a6474a4731bdf3c151a4b
dlydka <lydkadm@gmail.com>
2023-11-13 08:33:15 +0100

    Functional tests fix and indentation fix

--- functional/subscription_order_test.py
+++ functional/subscription_order_test.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 
 class SubscriptionOrderTests(unittest.TestCase):
     def setUp(self):
-        os.environ['BASE_URL'] = 'http://127.0.0.1:5000/'
         self.BASE_URL = os.getenv("BASE_URL", "")
         self.driver = webdriver.Chrome()
 
@@ -61,41 +60,10 @@ class SubscriptionOrderTests(unittest.TestCase):
 
         self.driver.get(os.path.join(self.BASE_URL, "edit-subscription"))
 
-        self.driver.find_element(by=By.LINK_TEXT, value="Strona Główna").click()        # click link to return to main page
+        self.driver.find_element(by=By.LINK_TEXT, value="Strona główna").click()        # click link to return to main page
 
         assert self.driver.title == "Paint Drying"
         assert self.driver.current_url == self.BASE_URL
-
-    def test_subscription_order_email_required(self):
-        self.driver.get(os.path.join(self.BASE_URL, "register"))
-        self.driver.find_element(by=By.ID, value="username").send_keys("ccc")
-        self.driver.find_element(by=By.ID, value="name").send_keys("ccc")
-        self.driver.find_element(by=By.ID, value="surname").send_keys("ccc")
-        self.driver.find_element(by=By.ID, value="age").send_keys(11)
-        self.driver.find_element(by=By.ID, value="email").send_keys("c@c.c")
-        self.driver.find_element(by=By.ID, value="gender-1").click()
-        self.driver.implicitly_wait(0.5)
-        element = self.driver.find_element(by=By.ID, value="submit")  # submit form
-        self.driver.execute_script("arguments[0].click();", element)
-
-        self.driver.implicitly_wait(0.5)
-        self.driver.get(os.path.join(self.BASE_URL, "login"))
-
-        self.driver.find_element(by=By.ID, value="email").send_keys("c@c.c")
-        self.driver.find_element(by=By.ID, value="submit").click()  # submit form
-
-        self.driver.get(os.path.join(self.BASE_URL, "edit-subscription"))
-
-        self.driver.implicitly_wait(0.5)
-
-        self.driver.find_element(by=By.ID, value="subscription_level-0").click()
-        self.driver.find_element(by=By.ID, value="submit").click()
-
-        required = self.driver.find_element(by=By.ID, value="email").get_attribute("required")
-        assert required == "true"
-
-        assert self.driver.title == "Order Subscription - Paint Drying"
-        assert self.driver.current_url == os.path.join(self.BASE_URL, "subscribe")  # check if not redirected
 
     def test_subscription_order_sub_level_required(self):
         self.driver.get(os.path.join(self.BASE_URL, "register"))
@@ -115,10 +83,9 @@ class SubscriptionOrderTests(unittest.TestCase):
         self.driver.find_element(by=By.ID, value="email").send_keys("d@d.d")
         self.driver.find_element(by=By.ID, value="submit").click()  # submit form
 
-        self.driver.get(os.path.join(self.BASE_URL, "subscribe"))
+        self.driver.get(os.path.join(self.BASE_URL, "edit-subscription"))
 
-        self.driver.find_element(by=By.ID, value="email").send_keys("d@d.d")
         self.driver.find_element(by=By.ID, value="submit").click()
 
-        assert self.driver.title == "Order Subscription - Paint Drying"
-        assert self.driver.current_url == os.path.join(self.BASE_URL, "subscribe")
+        assert self.driver.title == "Edit Subscription - Paint Drying"
+        assert self.driver.current_url == os.path.join(self.BASE_URL, "edit-subscription")
